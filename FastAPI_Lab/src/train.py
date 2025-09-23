447810ce14b5133fc48425606236585384d98fc1
Harish Padmanabhan <89925610+harish2412@users.noreply.github.com>
2025-09-22 22:49:10 -0400

    Updates train.py

--- FastAPI_Lab/src/train.py
+++ FastAPI_Lab/src/train.py
@@ -1,19 +1,28 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 import joblib
+from pathlib import Path
 from data import load_data, split_data
+
+MODEL_PATH = (Path(__file__).resolve().parent.parent / "model" / "breast_rf.pkl")
 
 def fit_model(X_train, y_train):
     """
-    Train a Decision Tree Classifier and save the model to a file.
-    Args:
-        X_train (numpy.ndarray): Training features.
-        y_train (numpy.ndarray): Training target values.
+    Train a Random Forest Classifier and save the model to a file.
     """
-    dt_classifier = DecisionTreeClassifier(max_depth=3, random_state=12)
-    dt_classifier.fit(X_train, y_train)
-    joblib.dump(dt_classifier, "../model/iris_model.pkl")
+    rf_classifier = RandomForestClassifier(n_estimators=1000,
+        max_depth=None,
+        min_samples_leaf=2,
+        class_weight=None,
+        n_jobs=-1,
+        random_state=12)
+    rf_classifier.fit(X_train, y_train)
+    
+    MODEL_PATH.parent.mkdir(parents=True, exist_ok=True)
+    joblib.dump(rf_classifier, MODEL_PATH)
+    return rf_classifier
 
 if __name__ == "__main__":
     X, y = load_data()
     X_train, X_test, y_train, y_test = split_data(X, y)
-    fit_model(X_train, y_train)
+    rf_classifier = fit_model(X_train, y_train)
